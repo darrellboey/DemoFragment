d7d1b627dab2afdc0f5911317c1b0dafc23d3dda
darrellboey <15056233@myrp.edu.sg>
2017-06-06 11:16:13 +0800

    adding 2nd fragment xml

--- app/src/main/java/com/example/a15056233/demofragment/MainActivity.java
+++ app/src/main/java/com/example/a15056233/demofragment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a15056233.demofragment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+
+        ft.commit();
     }
 }
+
+
